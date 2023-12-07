6b640ab7599aa123de51c354b5bbc3156f1f4ddf
nishaaa25 <sahnisha2505@gmail.com>
2023-12-07 10:37:53 +0530

    created .htaccess

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import AboutSection from "./components/Sections/AboutSection";
 import HomeSection from "./components/Sections/HomeSection";
-import { createBrowserRouter, } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import PortFolioSection from "./components/Sections/PortFolioSection";
 
@@ -14,25 +14,27 @@ const AppLayout = () => {
   );
 };
 
-const AppRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <HomeSection/>,
-      },
-      {
-        path: "/about",
-        element: <AboutSection/>
-      },
-      {
-        path: "/portfolio",
-        element: <PortFolioSection/>
-      }
-    ],
-  },
-]);
+const AppRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <HomeSection />,
+        },
+        {
+          path: "/about",
+          element: <AboutSection />,
+        },
+        {
+          path: "/portfolio",
+          element: <PortFolioSection />,
+        },
+      ],
+    },
+  ],
+);
 
 export { AppRouter };
